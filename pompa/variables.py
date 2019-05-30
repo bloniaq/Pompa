e8fc76676757bcc244c3ea8864129db3b0c0b299
bloniaq <bloniaq@op.pl>
2019-05-30 21:34:27 +0200

    work in progress

--- pompa/variables.py
+++ pompa/variables.py
@@ -8,7 +8,8 @@ class Variable():
 
     load_flag = False
 
-    def __init__(self, app, ui_variable, data_id):
+    def __init__(self, app, ui_variable, data_id, fig_depend):
+        self.fig_depend = fig_depend
         self.app = app
         self.builder = app.builder
         # self.tkvars = app.builder.tkvariables
@@ -32,17 +33,35 @@ class Variable():
         ui_content = self.ui_var.get()
         setattr(self, attr, ui_content)
 
-        if self.fig_depend and not load_flag:
-            try:
-                self.app.update_calculations()
-                # TODO:
-                # Distinct drawings base on fig_depend value
-                self.app.draw_auxillary_figures()
-            except (AttributeError, TypeError) as e:
-                log.error('Error {}'.format(e))
+        if self.fig_depend and not self.load_flag:
+            self.app.update_fig_calculations()
+            self.app.prepare_figure(self.fig_depend)
+            # TODO:
+            # write those functions
 
     def load_data(self, data_dict):
         self.load_flag = True
         if self.dan_id in data_dict:
             self.value = data_dict[self.dan_id]
         self.load_flag = False
+
+
+class P_Int(Variable, int):
+
+    def __new__(cls, app, value, ui_variable, data_id, fig_depend=False):
+        return int.__new__(cls, value)
+
+    def __init__(self, app, value, ui_variable, data_id, fig_depend=False):
+        Variable.__init__(app, ui_variable, data_id, fig_depend)
+
+    def __setattr__(self, attr, value):
+        if attr != 'value':
+            self.__dict__[attr] = value
+        else:
+            self.__dict__['value'] = value
+            self.ui_var.set(self.value)
+            if self.fig_depend and not self.load_flag:
+                try:
+                    self.app.draw_auxillary_figures()
+                except (AttributeError, TypeError) as e:
+                    log.error('Error {}'.format(e))
