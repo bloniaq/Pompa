653f15898cf514cac73d6f0dae96e9b7349b0005
bloniaq <bloniaq@op.pl>
2021-06-01 20:16:57 +0200

    hydr_cond module docstrings improved

--- pompa/models/hydr_conditions.py
+++ pompa/models/hydr_conditions.py
@@ -2,7 +2,28 @@ import pompa.models.variables as v
 
 
 class HydrConditions():
-    """
+    """Class used to keep hydraulic conditions of sewage pumping station
+
+    Attributes
+    ----------
+    ord_terrain : FloatVariable
+        The ordinate of terrain near sewage pumping station
+    ord_inlet : FloatVariable
+        The ordinate of lowest inlet pipe
+    ord_outlet : FloatVariable
+        The ordinate of outlet pipe
+    ord_bottom : FloatVariable
+        Assumed ordinate of bottom of well
+    ord_highest_point : FloatVariable
+        The highest point on pipe rout ordinate
+    ord_upper_level : FloatVariable
+        The ordinate of end of outside pipe(s)
+    reserve_height : FloatVariable
+        Assumed distance between inlet ordinate and alarm ordinate
+    inflow_max : FlowVariable
+        Assumed maximum inflow to sewage pumping station
+    inflow_min : FlowVariable
+        Assumed minimum inflow to sewage pumping station
     """
 
     def __init__(self):
