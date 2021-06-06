87158125d2661aed1adeb39f81a3ea0f128f52ef
bloniaq <bloniaq@op.pl>
2021-06-06 22:05:39 +0200

    workpoint u tests merged func file deleted

--- tests/unit/models/test_workpoint.py
+++ tests/unit/models/test_workpoint.py
@@ -31,3 +31,16 @@ def test_full_loss_poly(workpoint_dummy_1pump):
     result_poly = wpoint._full_loss_poly()
     assert isinstance(result_poly, np.ndarray)
     np.testing.assert_equal(result_poly, exp_pipeset_poly)
+
+def test_calculate(workpoint_dummy_1pump):
+    wpoint = workpoint_dummy_1pump
+    exp_height = 2
+    exp_flow = v.FlowVariable(3000, 'lps')
+    exp_geom_h = v.FloatVariable(6)
+    exp_ins_pipe_speed = 0.5
+    exp_out_pipe_speed = 0.25
+    assert wpoint.height == exp_height
+    assert wpoint.flow == exp_flow
+    assert wpoint.geom_h == exp_geom_h
+    assert wpoint.ins_pipe_v == exp_ins_pipe_speed
+    assert wpoint.out_pipe_v == exp_out_pipe_speed
