2ba83cd1892032fe57bd9ad4282d9c5a3c6df9f3
bloniaq <j.a.blonski@outlook.com>
2021-06-07 14:44:12 +0200

    docstrings corr

--- pompa/application.py
+++ pompa/application.py
@@ -4,8 +4,8 @@ import pompa.models.station as station
 import pompa.view.gui_tk as gui_tk
 
 
-class Application():
-    """docstring for Application"""
+class Application:
+    """Class used for instantiation the Application"""
 
     def __init__(self):
 
@@ -13,6 +13,6 @@ class Application():
         self.gui_tkinter = gui_tk.Gui()
 
     def run_gui(self):
-        """ Makes infinite loop
-        """
+        """Make infinite loop of GUI"""
+
         self.gui_tkinter.mainwindow.mainloop()
--- tests/unit/test_application.py
+++ tests/unit/test_application.py
@@ -1,12 +1,12 @@
 import pompa.application as pompa
 
+class Test_Application:
 
-def test_app_init():
+    def test_app_init(self):
 
-    app = pompa.Application()
-    assert isinstance(app, pompa.Application)
+        app = pompa.Application()
+        assert isinstance(app, pompa.Application)
 
-
-def test_gui_tkinter():
-    app = pompa.Application()
-    assert isinstance(app.gui_tkinter, pompa.gui_tk.Gui)
+    def test_gui_tkinter(self):
+        app = pompa.Application()
+        assert isinstance(app.gui_tkinter, pompa.gui_tk.Gui)
