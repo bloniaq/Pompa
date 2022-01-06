0140a12fce81e24a1af47c6882e0493f9da9eedd
bloniaq <j.a.blonski@outlook.com>
2022-01-06 12:51:57 +0100

    id wrap working

---
+++ pompa/view/variables.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+
+
+class ViewVariable:
+    """ Abstract class intended to provide additional id parameter for
+    subclassed tkinter variables."""
+
+    def __init__(self, var_id):
+        self.id = var_id
+
+
+class StringVar(tk.StringVar, ViewVariable):
+    """Class to wrap id parameter around standard tk.StringVar """
+
+    def __init__(self, id, *args, **kwargs):
+        ViewVariable.__init__(self, id)
+        tk.StringVar.__init__(self, *args, **kwargs)
+
+
+class IntVar(tk.IntVar, ViewVariable):
+    """Class to wrap id parameter around standard tk.IntVar"""
+
+    def __init__(self, id, *args, **kwargs):
+        ViewVariable.__init__(self, id)
+        tk.IntVar.__init__(self, *args, **kwargs)
+
+class DoubleVar(tk.DoubleVar, ViewVariable):
+    """Class to wrap id parameter around standard tk.DoubleVar"""
+
+    def __init__(self, id, *args, **kwargs):
+        ViewVariable.__init__(self, id)
+        tk.DoubleVar.__init__(self, *args, **kwargs)
+
