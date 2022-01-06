586ac6332915857d101876726d0bb90cb69d68de
bloniaq <j.a.blonski@outlook.com>
2022-01-06 13:40:57 +0100

    base sketch

--- pompa/view/gui_tk.py
+++ pompa/view/gui_tk.py
@@ -1,6 +1,11 @@
+import pompa.view.buttonframe as bf
+import pompa.view.dataframe as df
+import pompa.view.pipeframe as pipef
+import pompa.view.pumpframe as pumpf
+import pompa.view.variables as vv
+
 import tkinter as tk
 from tkinter import ttk
-import pompa.view.buttonframe as bf
 
 
 class View:
@@ -9,8 +14,58 @@ class View:
         self.root = tk.Tk()
         self.gui = Gui(self.root)
 
+        self.callbacks = {}
+
+        self._create_widget_aliases()
+        self._create_view_variables()
+
     def run(self):
         self.root.mainloop()
+
+    def add_callback(self, key, method):
+        self.callbacks[key] = method
+
+    def add_port(self, port):
+        self.values_port = port
+
+    def bind_callbacks(self):
+        self.load_button.config(command=self.callbacks['load_data'])
+        self.save_button.config(command=self.callbacks['save_data'])
+        self.unit_radio_meters.config(command=self.callbacks['units'])
+        self.unit_radio_liters.config(command=self.callbacks['units'])
+        self.safety_radio_econ.config(command=self.callbacks['safety'])
+        self.safety_radio_opti.config(command=self.callbacks['safety'])
+        self.safety_radio_safe.config(command=self.callbacks['safety'])
+        self.mode_radio_check.config(command=self.callbacks['mode'])
+        self.mode_radio_minimal.config(command=self.callbacks['mode'])
+        self.calc_button.config(command=self.callbacks['calculate'])
+
+    def _send_value(self, widget):
+        variable = widget.variable
+        id = variable.id
+        value = variable.get()
+        return self.values_port(id, value)
+
+    def _create_widget_aliases(self):
+        self.load_button = self.gui.buttonframe.load_button
+        self.save_button = self.gui.buttonframe.save_button
+        self.unit_radio_meters = self.gui.buttonframe.meters_radio
+        self.unit_radio_liters = self.gui.buttonframe.liters_radio
+        self.safety_radio_econ = self.gui.buttonframe.econ_radio
+        self.safety_radio_opti = self.gui.buttonframe.optim_radio
+        self.safety_radio_safe = self.gui.buttonframe.safe_radio
+        self.mode_radio_check = self.gui.buttonframe.checking_radio
+        self.mode_radio_minimal = self.gui.buttonframe.minimalisation_radio
+        self.calc_button = self.gui.buttonframe.calc_button
+
+    def _create_view_variables(self):
+        self.vars = {
+            'unit': tk.StringVar(),
+            'safety': tk.StringVar(),
+            'mode': tk.StringVar(),
+            'inlet_min': tk.DoubleVar(),
+            'inlet_max': tk.DoubleVar()
+        }
 
 
 class Gui(tk.Frame):
@@ -19,85 +74,56 @@ class Gui(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.parent = parent
 
-        self.mainframe = Mainframe(self)
+        unit_var = tk.StringVar()
+        unit_var.set('meters')
+
+        self.mainframe = Mainframe(self, unit_var)
         self.mainframe.pack(expand=True,
-                            fill=tk.X,
+                            fill=tk.BOTH,
                             side=tk.LEFT,
                             padx=(20, 10), pady=20)
         self.logoframe = Logo(self)
         self.logoframe.pack(side=tk.TOP,
                             padx=(10, 20), pady=(20, 10),
                             ipadx=10, ipady=10)
-        self.buttonframe = bf.Buttonframe(self)
+        self.buttonframe = bf.Buttonframe(self, unit_var, self._unit_change)
         self.buttonframe.pack(expand=True,
                               fill=tk.BOTH,
                               padx=(10, 20), pady=(10, 20),
                               ipadx=10, ipady=10)
         self.pack()
 
+    def _unit_change(self):
+        self.mainframe.update_units()
+
+
 class Mainframe(tk.Frame):
 
-    def __init__(self, parent):
+    def __init__(self, parent, unit_var):
         tk.Frame.__init__(self, parent)
         self.parent = parent
 
-        self.notebook = tk.ttk.Notebook(self)
-        self.notebook.pack()
-        self.first_frame = tk.ttk.Frame(self.notebook)
-        self.first_frame.pack()
-        self.notebook.add(self.first_frame, text='pierwsza ramka')
-        self.label = tk.Label(self.first_frame,
-                              justify=tk.LEFT,
-                              wraplength=400,
-                              text="""Primus, lotus sagas etiam imperium de salvus, pius itineris tramitem.
-Ausus de raptus genetrix, magicae candidatus!
-valebats sunt musas de primus particula.
-vitas sunt eleatess de gratis adelphis.
-Sunt nixuses captis altus, bi-color victrixes.
-impositio moris, tanquam fatalis habena.
-cur parma persuadere?
-Cum aonides congregabo, omnes imberes pugna flavum, dexter nomenes.
-Pess sunt lactas de alter eleates.
-altus liberi tandem attrahendams bubo est.
-cum pulchritudine favere, omnes ollaes experientia azureus, audax barcases.
-Valebats sunt hibridas de placidus repressor.
-a falsis, fluctui raptus assimilatio.
-germanus, gratis classiss aliquando acquirere de noster, bassus abactus. est bi-color devirginato, cesaris.
-Hilotae de bi-color fraticinida, attrahendam magister!
-aususs sunt fugas de festus cacula.
-sunt absolutioes acquirere teres, rusticus itineris tramitemes.
-Sunt zirbuses examinare alter, lotus brodiumes.
-barbatus fortiss ducunt ad decor.
-nunquam anhelare nomen.
-Cum barcas experimentum, omnes abaculuses manifestum audax, germanus bullaes.
-Hippotoxota de fidelis cannabis, visum bubo!
-diatria de talis tumultumque, acquirere adiurator!
-cum accola credere, omnes guttuses amor bassus, clemens indexes.
-Absolutio de secundus mortem, anhelare turpis!
-detriuss messis, tanquam domesticus lapsus.
-verpa, visus, et capio. cum luba persuadere, omnes hilotaees contactus teres, raptus burguses.
-Domuss sunt rationes de magnum omnia.
-talis verpa aegre visums lura est.
-gratis lacta virtualiter tractares hydra est.
-Gratis triticum sed mire consumeres omnia est.
-a falsis, mortem raptus planeta.
-nunquam fallere itineris tramitem.
-Cum guttus messis, omnes elevatuses dignus fortis, velox armariumes.
-Mirabilis olla rare dignuss orgia est.
-nuptias sunt ollas de albus devirginato.
-brevis, placidus gloss acceleratrix imperium de flavum, alter repressor.
-Lixas sunt fortiss de regius mons.
-a falsis, zirbus gratis hilotae.
-est primus zelus, cesaris.
-Ausus de raptus genetrix, magicae candidatus!
-valebats sunt musas de primus particula.
-vitas sunt eleatess de gratis adelphis.
-Sunt nixuses captis altus, bi-color victrixes.
-impositio moris, tanquam fatalis habena.
-cur parma persuadere?
-""")
+        self.notebook = Notebook(self)
+        self.notebook.pack(expand=True,
+                           fill=tk.BOTH)
+        self.dataframe = df.Dataframe(self.notebook, unit_var)
+        self.pipeframe = pipef.Pipeframe(self.notebook)
+        self.pumpframe = pumpf.Pumpframe(self.notebook, unit_var)
+        self.notebook.add(self.dataframe, text='  Dane Projektowe  ')
+        self.notebook.add(self.pipeframe, text="  Przewody  ")
+        self.notebook.add(self.pumpframe, text='  Pompa  ')
 
-        self.label.pack(ipadx=20, ipady=20)
+    def update_units(self):
+        self.pumpframe.update_units()
+        self.dataframe.update_units()
+
+
+class Notebook(tk.ttk.Notebook):
+
+    def __init__(self, parent):
+        tk.ttk.Notebook.__init__(self, parent)
+        self.parent = parent
+
 
 class Logo(tk.Frame):
 
@@ -107,18 +133,19 @@ class Logo(tk.Frame):
                           bd=2)
         self.parent = parent
         self.logo_label = tk.Label(self, font=("Courier New", 10, "bold"),
-            text="""POMPA   POMPA  POMPA  POMPA  POMPA
+                                   text="""POMPA   POMPA  POMPA  POMPA  POMPA
 __________________________________
 
- ****   ****  *     * ****   ***
+****   ****  *     * ****   ***
  *   * *    * * * * * *   * *   *
  ****  *    * *  *  * ****  *****
  *     *    * *     * *     *   *
  *      ****  *     * *     *   *
 __________________________________
 
-POMPA    Wersja 2.01/2021r   POMPA""")
+POMPA    Wersja 2.02/2022r   POMPA""")
         self.logo_label.pack(expand=True)
+
 
 if __name__ == "__main__":
     view = View()
