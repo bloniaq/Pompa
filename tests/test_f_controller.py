1a073dc05294eade108234d69d570435b5209555
bloniaq <j.a.blonski@outlook.com>
2022-01-06 20:23:20 +0100

    rewriting view to model first test added

--- tests/test_f_controller.py
+++ tests/test_f_controller.py
@@ -1,3 +1,16 @@
+import tkinter as tk
+
+import pompa.application as app_module
+
+
 class Test_Controller:
 
-    pass
+    def test_rewriting_doublevar_data_from_view_to_model(self):
+        controller = app_module.Application()
+
+        test_value = 11.09
+
+        controller.view.ord_terrain_entry.delete(0, tk.END)
+        controller.view.ord_terrain_entry.insert(0, test_value)
+
+        assert controller.model.ord_terrain.get() == test_value
