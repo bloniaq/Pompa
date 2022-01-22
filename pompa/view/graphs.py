5cc621948694520becce6a7e20daf09e37037a0a
bloniaq <j.a.blonski@outlook.com>
2022-01-22 14:54:33 +0100

    dimensions in pixels defined

--- pompa/view/graphs.py
+++ pompa/view/graphs.py
@@ -3,12 +3,21 @@ from matplotlib.figure import Figure
 
 
 class PipesGraph(FigureCanvasTkAgg):
+    # DPI rate is a monitor property
+    DPI = 81
+    # Figure dimensions in pixels
+    PIX_X = 690
+    PIX_Y = 510
 
     def __init__(self, master):
-        fig = Figure(figsize=(5, 4), dpi=100)
+        # This formula lets define figure dimensions in pixels
+        fig = Figure(figsize=(self.PIX_X/self.DPI, self.PIX_Y/self.DPI),
+                     dpi=self.DPI)
         FigureCanvasTkAgg.__init__(self, fig, master)
 
         self.draw()
 
     def pack(self, *args, **kwargs):
+        """Zastępuje pack() tak, żeby można było traktować obiekt jak widget tk
+        """
         return self.get_tk_widget().pack(*args, **kwargs)
