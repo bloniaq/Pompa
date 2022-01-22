76edf9eb1dd185e2efb2a950f3564c64ebeeb983
bloniaq <j.a.blonski@outlook.com>
2022-01-22 17:55:28 +0100

    composition instead of inheritance

--- pompa/view/graphs.py
+++ pompa/view/graphs.py
@@ -1,8 +1,9 @@
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 
 
-class PipesGraph(FigureCanvasTkAgg):
+class PipesGraph:
     # DPI rate is a monitor property
     DPI = 81
     # Figure dimensions in pixels
@@ -11,13 +12,24 @@ class PipesGraph(FigureCanvasTkAgg):
 
     def __init__(self, master):
         # This formula lets define figure dimensions in pixels
-        fig = Figure(figsize=(self.PIX_X/self.DPI, self.PIX_Y/self.DPI),
-                     dpi=self.DPI)
-        FigureCanvasTkAgg.__init__(self, fig, master)
+        fig, axs = plt.subplots(figsize=(self.PIX_X/self.DPI, self.PIX_Y/self.DPI),
+                                dpi=self.DPI)
+        self.plot = fig.add_subplot(111)
+
+        self.canvas = FigureCanvasTkAgg(fig, master)
 
         self.draw()
 
     def pack(self, *args, **kwargs):
         """Zastępuje pack() tak, żeby można było traktować obiekt jak widget tk
         """
-        return self.get_tk_widget().pack(*args, **kwargs)
+        return self.canvas.get_tk_widget().pack(*args, **kwargs)
+
+    def pass_data(self, data):
+
+        # OBRÓBKA DANYCH
+
+        self.draw()
+
+    def draw(self):
+        pass
