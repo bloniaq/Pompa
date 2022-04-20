a56af3b42b29d59e1e841c3ee61c4fe328b52087
bloniaq <j.a.blonski@outlook.com>
2022-04-20 23:58:06 +0200

    corrected objects

--- tests/gui/test_controller.py
+++ tests/gui/test_controller.py
@@ -12,4 +12,4 @@ class Test_Controller:
             controller.view.ord_terrain_entry.delete(0, tk.END)
             controller.view.ord_terrain_entry.insert(0, test_value)
 
-            assert controller.model.ord_terrain.get() == test_value
+            assert controller.model.hydr_cond.ord_terrain.get() == test_value
