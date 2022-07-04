eb64ac3fbad82f0ddba9935717c5bada4356a192
bloniaq <j.a.blonski@outlook.com>
2022-07-04 22:34:30 +0200

    VMVar initialisation unit test

--- tests/unit/test_application.py
+++ tests/unit/test_application.py
@@ -1,7 +1,26 @@
 import pompa.application as pompa
 import pompa.view as view
 
-class Test_Application:
+
+class TestVMVar:
+
+    def test_instance_init(self):
+        test_name = "variable"
+        test_id = 0
+        test_type = "double"
+        test_def_value = 5.45
+
+        var = pompa.VMVar(test_name, test_id, test_type, test_def_value)
+
+        assert var.name == test_name
+        assert var.id == test_id
+        assert var.type == test_type
+        assert var.default_value == test_def_value
+        assert var.viewvar is None
+        assert var.modelvar is None
+
+
+class TestApplication:
 
     def test_app_init(self):
         with pompa.Application() as app:
