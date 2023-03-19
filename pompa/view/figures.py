609ef25e77cbc26153e4abf1aae384f2c2c6db0b
bloniaq <j.a.blonski@outlook.com>
2023-03-19 07:27:25 +0100

    ResultGraph

--- pompa/view/figures.py
+++ pompa/view/figures.py
@@ -3,12 +3,12 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
 from matplotlib.ticker import FormatStrFormatter
 
+X_MULTIPLIER = {'lps': 1000, 'm3ps': 1, 'm3ph': 3600}
+
 
 class DynamicGraph:
     # DPI rate is a monitor property
     DPI = 81
-
-    X_MULTIPLIER = {'lps': 1000, 'm3ps': 1, 'm3ph': 3600}
 
     def __init__(self, master, pix_x, pix_y):
         fig = plt.figure(figsize=(pix_x / self.DPI, pix_y / self.DPI),
@@ -68,7 +68,7 @@ class PipesGraph(DynamicGraph):
             'y_out_pipe': self.draw_outside_pipe_plot,
             'y_coop': self.draw_both_pipe_plot
         }
-        x_mul = self.X_MULTIPLIER[unit]
+        x_mul = X_MULTIPLIER[unit]
         for figure in methods.keys():
             if data[figure]:
                 print(data[figure])
@@ -151,7 +151,7 @@ class PumpGraph(DynamicGraph):
 
         self.clear()
 
-        x_mul = self.X_MULTIPLIER[unit]
+        x_mul = X_MULTIPLIER[unit]
 
         if data['y_p_points']:
             self.draw_points(data['y_p_points'], x_mul)
@@ -225,9 +225,57 @@ class ResultGraph:
         self.data = data
         self.plot = fig.add_subplot(111)
         self.canvas = FigureCanvasTkAgg(fig, parent)
+        self.draw(data)
         self.canvas.draw()
 
     def pack(self, *args, **kwargs):
         """Zastępuje pack() tak, żeby można było traktować obiekt jak widget tk
         """
         return self.canvas.get_tk_widget().pack(*args, **kwargs)
+
+    def clear(self):
+        self.plot.clear()
+        self.canvas.draw()
+
+    def draw(self, data):
+        unit = 'lps'
+        x_mul = X_MULTIPLIER[unit]
+
+        self.draw_characteristic(data['x'], data['pumpset'], x_mul)
+        self.draw_efficiency(data['pump_eff'], data['pumpset'], x_mul)
+        self.draw_pipeset_stop(x_mul * data['x'], data['pipeset_stop'](data['x']))
+        self.draw_workpoints(data['workpoint_start'], data['workpoint_stop'], x_mul)
+        self.set_plot_grids(x_mul * data['x'], unit)
+
+    def draw_characteristic(self, x, y, mul):
+        self.plot.plot(x * mul, y(x), 'b-', label='charakterystyka pompy')
+
+    def draw_efficiency(self, values, y_pump, mul):
+        eff_from_x = mul * values[0]
+        eff_from_y = y_pump(eff_from_x / mul)
+        eff_to_x = mul * values[1]
+        eff_to_y = y_pump(eff_to_x / mul)
+        self.plot.plot([eff_from_x, eff_from_x], [0, eff_from_y], 'r--')
+        self.plot.plot([eff_to_x, eff_to_x], [0, eff_to_y], 'r--',
+                       label='obszar maks. wydajności pompy')
+
+    def draw_pipeset_stop(self, x, y):
+        self.plot.plot(x, y, 'g-', label='zespół przewodów (zatrzymanie pracy)')
+
+    def draw_pipeset_start(self, x, y):
+        self.plot.plot(x, y, color='gray', linestyle='--',
+                       label='zespół przewodów (uruchomienie)')
+
+    def draw_workpoints(self, wp_start, wp_stop, xmul):
+        # self.plot.plot(wp_start[0] * xmul, wp_start[1], 'bo')
+        self.plot.plot(wp_stop[0] * xmul, wp_stop[1], 'ro',
+                       label='punkt pracy (zatrzymanie pracy)')
+
+    def set_plot_grids(self, x, unit):
+        self.plot.grid(True, 'major', linestyle='--')
+        unit_bracket_dict = {'lps': '[l/s]', 'm3ph': '[m³/h]'}
+        self.plot.set_xlabel('Przepływ Q {}'.format(unit_bracket_dict[unit]))
+        self.plot.set_ylabel('Ciśnienie [m. sł. c.]')
+        self.plot.set_xlim(left=x[0], right=x[-1])
+        self.plot.set_ylim(bottom=0)
+        self.plot.legend(fontsize='small')
