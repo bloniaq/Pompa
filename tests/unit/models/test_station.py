2d0cc3802b936281da4c8945f8df5cbb3609eb21
bloniaq <j.a.blonski@outlook.com>
2023-09-05 00:08:27 +0200

    check_well_area organized better

--- tests/unit/models/test_station.py
+++ tests/unit/models/test_station.py
@@ -14,84 +14,6 @@ def test_calculation_run(station_2):
         station_2.pumpsystem, pompa.models.station.pumpsystem.PumpSystem)
 
 
-def test_check_well_area_station_2_round_singlerow(station_2):
-    # essential_data_s2 = {
-    #     'contour': 0.3,
-    #     'well_diam': 2.5,
-    #     'shape': 'round',
-    #     'config': 'singlerow',
-    #     'pump_contour': pump_c
-    #     }
-    all_pumps_count = 8
-    assert station_2.check_well_area_for_pumps(all_pumps_count)
-    all_pumps_count = 9
-    assert not station_2.check_well_area_for_pumps(all_pumps_count)
-
-
-def test_check_well_area_station_2_round_optimal(station_2):
-    s = station_2
-    s.well.config.set('optimal')
-    s.well.diameter.set(1.7)
-
-    # essential_data = {
-    #     'contour': 0.3,
-    #     'well_diam': 1.7,
-    #     'shape': 'round',
-    #     'config': 'optimal',
-    #     'pump_contour': pump_c
-    #     }
-
-    all_pumps_count = 5
-    assert s.check_well_area_for_pumps(all_pumps_count)
-    all_pumps_count = 6
-    assert not s.check_well_area_for_pumps(all_pumps_count)
-
-
-def test_check_well_area_station_2_rectangular_optimal(station_2):
-    s = station_2
-    s.well.shape.set('rectangle')
-    s.well.width.set(3)
-    s.well.length.set(4)
-    s.well.config.set('optimal')
-
-    # essential_data = {
-    #     'contour': 0.3,
-    #     'well_width': 3,
-    #     'well_length': 4,
-    #     'shape': 'rectangle',
-    #     'config': 'optimal',
-    #     'pump_contour': safe_pump_c=0.6
-    #     }
-
-    all_pumps_count = 30
-    assert s.check_well_area_for_pumps(all_pumps_count)
-    all_pumps_count = 31
-    assert not s.check_well_area_for_pumps(all_pumps_count)
-
-
-def test_check_well_area_station_2_rectangular_singlerow(station_2):
-    s = station_2
-    s.well.shape.set('rectangle')
-    s.well.width.set(2.2)
-    s.well.length.set(1.8)
-    s.pump_type.contour.set(0.5)
-    s.well.config.set('singlerow')
-
-    # essential_data = {
-    #     'contour': 0.5,
-    #     'well_width': 2.2,
-    #     'well_length': 1.8,
-    #     'shape': 'rectangle',
-    #     'config': 'singlerow',
-    #     'pump_contour': pump_c=0.5
-    #     }
-
-    all_pumps_count = 4
-    assert s.check_well_area_for_pumps(all_pumps_count)
-    all_pumps_count = 5
-    assert not s.check_well_area_for_pumps(all_pumps_count)
-
-
 def test_validate_dead_volume_under_inlet(station_2):
     s = station_2
     s.pump_type.suction_level.set(0.6)
@@ -105,6 +27,39 @@ def test_min_ins_pipe_length(station_2):
     assert s.validate_min_ins_pipe_length()
     s.ins_pipe.length.set(2)
     assert not s.validate_min_ins_pipe_length()
+
+
+# below testing CHECKING if pumps fit in well
+
+
+pumpsystems_check_area = [
+    # shape, config, contour, well_dim, pumps, result
+    ('round', 'singlerow', 0.3, 2.5, 8, True),
+    ('round', 'singlerow', 0.3, 2.5, 9, False),
+    ('round', 'optimal', 0.3, 1.7, 5, True),
+    ('round', 'optimal', 0.3, 1.7, 6, False),
+    ('rectangle', 'optimal', 0.3, (3.0, 4.0), 30, True),
+    ('rectangle', 'optimal', 0.3, (3.0, 4.0), 31, False),
+    ('rectangle', 'singlerow', 0.5, (2.2, 1.8), 4, True),
+    ('rectangle', 'singlerow', 0.5, (2.2, 1.8), 5, False),
+]
+
+
+@pytest.mark.parametrize('shape, config, contour, well_dim, pumps, result',
+                         pumpsystems_check_area)
+def test_check_well_area(station_2, shape, config, contour, well_dim, pumps,
+                         result):
+    station_2.well.shape.set(shape)
+    station_2.well.config.set(config)
+    station_2.pump_type.contour.set(contour)
+    if station_2.well.shape.value == 'round':
+        station_2.well.diameter.set(well_dim)
+    elif station_2.well.shape.value == 'rectangle':
+        station_2.well.width.set(well_dim[0])
+        station_2.well.length.set(well_dim[1])
+
+    assert station_2.check_well_area_for_pumps(pumps) == result
+
 
 # below testing CALCULATIONS of minimal dimensions
 
@@ -132,8 +87,7 @@ pumpsystems_min_d = [
     ('rectangle', 'optimal', 0.7, 9, (1.87, 5.00)),
     ('rectangle', 'optimal', 0.7, 10, (1.87, 5.50)),
     ('rectangle', 'optimal', 0.2, 5, (1.00, 2.50)),  # minimal width + length
-    ('rectangle', 'optimal', 0.3, 5, (1.12, 2.50)),  # minimal length
-
+    ('rectangle', 'optimal', 0.3, 5, (1.12, 2.50))  # minimal length
 ]
 
 
