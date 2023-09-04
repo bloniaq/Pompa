2ed02cbe5c9a8d48249cb88b9c0a04d6b48c5e35
bloniaq <j.a.blonski@outlook.com>
2023-09-04 20:40:32 +0200

    pycharm problems fixed

--- tests/unit/models/test_station.py
+++ tests/unit/models/test_station.py
@@ -11,6 +11,7 @@ def test_calculation_run(station_2):
     assert isinstance(
         station_2.pumpsystem, pompa.models.station.pumpsystem.PumpSystem)
 
+
 def test_min_well_dim_round_singlerow():
     station = pompa.models.station.Station()
     station.well.shape.set("round")
@@ -26,6 +27,7 @@ def test_min_well_dim_round_singlerow():
     complete_pump_number = 2
     station.pump_type.contour.set(0.2)
     assert station.min_well_dimension(complete_pump_number) == 1.5
+
 
 def test_min_well_dim_rect_singlerow():
     station = pompa.models.station.Station()
@@ -50,6 +52,7 @@ def test_min_well_dim_rect_singlerow():
     station.pump_type.contour.set(2)
     assert station.min_well_dimension(complete_pump_number) == (2.6, 9.2)
 
+
 def test_min_well_dim_round_optimal():
     station = pompa.models.station.Station()
     station.well.shape.set("round")
@@ -69,6 +72,7 @@ def test_min_well_dim_round_optimal():
     station.pump_type.contour.set(0.2)
     complete_pump_number = 1
     assert station.min_well_dimension(complete_pump_number) == 1.5
+
 
 def test_min_well_dim_rectangle_optimal():
     station = pompa.models.station.Station()
@@ -92,6 +96,7 @@ def test_min_well_dim_rectangle_optimal():
     complete_pump_number = 5
     assert station.min_well_dimension(complete_pump_number) == (1.50, 1.50)
 
+
 def test_check_well_area_station_2_round_singlerow(station_2):
     # essential_data_s2 = {
     #     'contour': 0.3,
@@ -104,6 +109,7 @@ def test_check_well_area_station_2_round_singlerow(station_2):
     assert station_2.check_well_area_for_pumps(all_pumps_count)
     all_pumps_count = 9
     assert not station_2.check_well_area_for_pumps(all_pumps_count)
+
 
 def test_check_well_area_station_2_round_optimal(station_2):
     s = station_2
@@ -145,6 +151,7 @@ def test_check_well_area_station_2_rectangular_optimal(station_2):
     all_pumps_count = 31
     assert not s.check_well_area_for_pumps(all_pumps_count)
 
+
 def test_check_well_area_station_2_rectangular_singlerow(station_2):
     s = station_2
     s.well.shape.set('rectangle')
@@ -167,12 +174,14 @@ def test_check_well_area_station_2_rectangular_singlerow(station_2):
     all_pumps_count = 5
     assert not s.check_well_area_for_pumps(all_pumps_count)
 
+
 def test_validate_dead_volume_under_inlet(station_2):
     s = station_2
     s.pump_type.suction_level.set(0.6)
     assert s.validate_dead_volume_under_inlet()
     s.hydr_cond.ord_inlet.set(139.11)
     assert not s.validate_dead_volume_under_inlet()
+
 
 def test_min_ins_pipe_length(station_2):
     s = station_2
